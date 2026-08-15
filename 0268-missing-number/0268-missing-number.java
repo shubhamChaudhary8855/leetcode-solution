687de4a0ba34821feb41fc3ec761ac38107d029e
Shubham Chaudhary <chaudharyshubham84954@gmail.com>
2026-08-15 19:17:35 +0530

    Time: 0 ms (100%), Space: 47.5 MB (33.47%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int expected_sum = n*(n+1)/2;
+        int sum  =0;
+        for(int i=0;i<n;i++){
+            sum += nums[i];
+        }
+        return expected_sum - sum;
+    }
+}
