c71ab8be3173260ce0eb9d7c75d90533f8591660
Shubham Chaudhary <chaudharyshubham84954@gmail.com>
2026-09-07 12:33:04 +0530

    Time: 2 ms (62.81%), Space: 42.9 MB (51.76%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        boolean[] freq = new boolean[26];
+        for(int i=0;i<sentence.length();i++){
+            char ch = sentence.charAt(i);
+            freq[ch - 'a'] = true;
+        }
+        for(int i=0;i<26;i++){
+            if(!freq[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
